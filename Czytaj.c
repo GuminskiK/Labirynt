4530a86f9bfaffcf05f6c9e6acd1d707eb4ad7bb
KrzysztofG <krzys.gumi@gmail.com>
2024-03-16 14:31:46 +0100

    Dodano moduł Czytaj
    Dodano moduł Czytaj zawierający funkcje: czytaj - która wczytuje wiersze do tablicy, przepisz - która przepisuje dolne wiersze tablicy o jeden wiersz w górę.

---
+++ Czytaj.c
@@ -0,0 +1,58 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "Czytaj.h"
+
+void czytaj(char x[][3], FILE * f, int y){ //wczytuje wiersze do tablicy
+	
+	//zmienne do operacji
+        int c,i;
+
+        // za pierwszym razem wczytujemy 3 linie
+        if ( y == 0){
+
+                for ( int g = 0; g < 3; g++){
+
+                        i = 0;
+
+                        //iterujemy po calym wierszu az do konca albo entera
+                        while( (c = fgetc(f)) != EOF && c != '\n'){
+
+                                x[i][g] = c;
+
+                                //to jest while wiec samo sie nie zrobi
+                                i++;
+
+                        }
+                }
+
+        // potem mamy repis wiec wczytujemy tylko 1 dolna linijke
+        }else{
+
+                i = 0;
+
+                while( (c = fgetc(f)) != EOF && c != '\n'){
+
+                        x[i][2] = c;
+
+                        //while
+                        i++;
+
+               }
+        }
+}
+
+void przepisz(char x[][3], int kolumny){ //przepisuje dolne wiersze tablicy o jeden wiersz w gore
+
+	//zwykla petla 2 linie a potem kolumny kolumn
+        for (int g = 0; g < 2; g++){
+
+                for (int i = 0; i < kolumny; i++){
+
+                        //do wyzszego wiersza nizszy
+                        x[i][g] = x[i][g+1];
+                }
+        }
+
+}
+
+
---
+++ Czytaj.h
@@ -0,0 +1,7 @@
+#ifndef _Czytaj_
+#define _Czytaj_
+
+void czytaj(char x[][3], FILE * f, int y);
+void przepisz(char x[][3], int kolumny);
+
+#endif
