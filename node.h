4ee44ad1c4e2fce237ebcd130afc98f99bb5413a
KrzysztofG <krzys.gumi@gmail.com>
2024-03-24 10:18:45 +0100

    Dodano moduł node
    Dodano plik node.h, który przetrzymuje tylko strukturę node, z której złożony jest graf.

---
+++ node.h
@@ -0,0 +1,12 @@
+#ifndef _node_
+#define _node_
+
+typedef struct{
+
+        unsigned int ID: 17;
+        unsigned int Value: 14;
+        unsigned int Direction: 4;
+	unsigned int X: 3;
+}Node;
+
+#endif
